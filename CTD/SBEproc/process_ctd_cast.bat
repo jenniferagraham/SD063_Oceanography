5064b564ce9b203df2a7ea31d3f67cbd111a8e16
Jennifer Graham <jennifer.graham@sams.ac.uk>
2026-07-26 13:10:30 -0100

    Option to process with or without niskin sampling

--- CTD/SBEproc/process_ctd_cast.bat
+++ CTD/SBEproc/process_ctd_cast.bat
@@ -1,5 +1,14 @@
 @echo off
 set /p castno=Enter CTD number (three digits): 
 echo Processing SD063 cast %castno%
-sbebatch L:\work\scientific_work_areas\oceanography\CTD\SBEproc\process_ctd_cast.txt %castno%
+set /p bottles=Did you sample niskin bottles (y/n):
+if /i "%bottles%"=="y" (
+    echo Processing with niskin bottles ...
+    sbebatch L:\work\scientific_work_areas\oceanography\CTD\SBEproc\process_ctd_cast.txt %castno%
+) else if /i "%bottles%"=="n" (
+    echo WARNING: Processing with NO niskin bottles ...
+    sbebatch L:\work\scientific_work_areas\oceanography\CTD\SBEproc\process_ctd_castNoBott.txt %castno%
+) else (
+    echo ERROR: y or n required
+)
 echo Done!
