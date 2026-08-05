b88a012992fb6353abacf5a236d03b1b0cc38858
Jennifer Graham <jennifer.graham@sams.ac.uk>
2026-08-05 13:14:47 -0100

    Add cast number to legend, and fix position

--- CTD/plot_transects/plot_casts_T_S_repeats.m
+++ CTD/plot_transects/plot_casts_T_S_repeats.m
@@ -7,32 +7,41 @@ ctddata = [disk,'CTD\BASproc\'];
 cruise='SD063';
 load([ctddata,cruise,'_ctd.mat']);
 
-sectionfilename='repeat_3m_icefront';
-P = sdaSectionParams(sectionfilename);
+sectionfilename='repeat_3m_icefront';%'repeat_3m_icefront';
+%sectionfilename='repeat_3msillpeak'; %{'3macrosssill-1','3macrosssill-2'};
 
-ncasts = length(P.sectionlist);
+%sectionfilename='yoyo_3meastsill';
 
-blueScale = abyss(3); 
-orangeScale = autumn(ncasts-3);
 grey  = [0.55 0.55 0.55];
 
 ctds_times=[];
+cast_number=[];
 
 %Add in ice front repeat
+P = sdaSectionParams(sectionfilename);
+ncasts = length(P.sectionlist);
+blueScale = abyss(ncasts); 
+orangeScale = autumn(ncasts);
+
 for ii=1:ncasts
-    if ii<=3
+    if ii<3
         cols=blueScale(ii,:);
     else
-        cols=orangeScale(ii-3,:);
+        cols=orangeScale(ii,:);
     end
+
     ax= sd063_cast_plots(P.sectionlist(ii),cols);
     hold on;
     ctd_time=datetime(ctds(P.sectionlist(ii)).gtime);
     ctds_times=[ctds_times ctd_time];
+    cast_number=[cast_number P.sectionlist(ii)];
 end
 
-subplot(1,4,2)
-lgd=legend(string(ctds_times),'Location','SouthWest','FontSize',8);
+subplot(1,4,3:4)
+labels = string(ctds_times) + " (" + string(cast_number) + ")";
+legend(labels,'Location','northeast','FontSize',8);
+
+%lgd=legend([string(ctds_times),(cast_number)],'Location','SouthWest','FontSize',8);
 %,string(ctds_times(3)),string(ctds_times(4)),string(ctds_times(5)),string(ctds_times(6)),string(ctds_times(7)),'Location','SouthWest','FontSize',8)
 lgd.ItemTokenSize = [12 10];
 
