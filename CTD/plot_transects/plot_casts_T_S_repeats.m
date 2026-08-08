fac7e5b7ce9a58250d30e2f22952fe8eb72f2ce5
Jennifer Graham <jennifer.graham@sams.ac.uk>
2026-08-08 09:41:23 -0100

    Updates from night watch

--- CTD/plot_transects/plot_casts_T_S_repeats.m
+++ CTD/plot_transects/plot_casts_T_S_repeats.m
@@ -1,11 +1,12 @@
 %Script to plot all casts from ice front section for comparison
 %% add path 
+addpath L:\work\scientific_work_areas\oceanography\matlabF\
+   addpath([disk,'\GSWscripts\gsw_matlab_v3_06_16\'])
 close all; clear all;
 
 %if reF_station=[], it plots casts. If you give it a ref station, it plots
 %anomolies.
 ref_station=[];
-
 %turn off here as this is only used to plot envelope (using means and stds)
 plot_envelope=0;
 
@@ -13,41 +14,52 @@ if ispc
     addpath 'L:\work\scientific_work_areas\oceanography\CTD\Code'
     disk = ['L:\work\scientific_work_areas\oceanography\'];
     ctddata = [disk,'CTD\BASproc\'];
+    ctddata_old = [disk,'\Notes\PreviousDataProcessing\KANGGLAC_CTD_data\'];
    
 else
     addpath '/Volumes/legwork/scientific_work_areas/oceanography/CTD/Code/'
     disk = ['/Volumes/legwork/scientific_work_areas/oceanography/'];
     ctddata = [disk,'CTD/BASproc/'];
 end
+
+%run through different cruises
+cruises={'SD063','SK2514','SD041'};
 %% load CTD structure data 
  cruise='SD063';
 load([ctddata,cruise,'_ctd.mat']);
 
 %% select the sections 
-%sectionfilename='repeat_3m_icefront';%'repeat_3m_icefront';
-%sectionfilename={'repeat_3moutermouth'};
  sectionfilename={'repeat_3mmmouthsectionrepeats'};
 
-%sectionfilename={'3mbeaksouth-1','3mbeaksouth-2'};
-
-% sectionfilename='3mtransect';
-% sectionfilename='3mtransect_anomalies'; 
-% sectionfilename='3micefronttest';
-% sectionfilename ='3micefronttowyo';
+sectionfilename={'kgtrough-1','kgtrough-2'};
+%sectionfilename={'kgtrough-1'};
 
 grey  = [0.55 0.55 0.55];
 
 ctds_times=[];
 cast_number=[];
 
-for m=1
+for m=1:2
 %Add in ice front repeat
 P = sdaSectionParams(sectionfilename{m}); % function that needs to be in the same folder 
 ncasts = length(P.sectionlist);
 stns=P.sectionlist;
 blueScale = abyss(ncasts); 
 orangeScale = autumn(ncasts);
-phaseScale=hsv(ncasts);
+greenScale = summer(ncasts);
+phaseScale=hsv(5);
+RosieScale =[
+    0.00 0.35 0.75   % blue
+    0.00 0.60 0.30   % green
+%    0.00 0.75 0.75   % turquoise
+    0.00 0.00 0.00   % black
+    0.90 0.45 0.05    % pink
+    0.85 0.05 0.05   % red
+];
+
+% Make lighter versions for the repeats
+phaseScale_light = 0.5 + 0.5*phaseScale;
+RosieScale_light = 0.5 + 0.5*RosieScale;
 
 allstations=[ctds.station];
 ind=zeros(size(stns));
@@ -64,17 +76,18 @@ ctds_plot=ctds(ind);
 %% loop the sections 
 for ii=1:ncasts
     if m==1
-        cols=blueScale(ii,:);
+     %   if ii<6
+        %    cols=orangeScale(ii,:);  
+      %  else
+        cols=RosieScale(ii,:);
+        line_style='-'
     elseif m==2
-             cols=orangeScale(ii,:);
+             cols=RosieScale_light(ii,:);
+             line_style='-'
     end
-   % else
-    %    cols=[0.0 0.0 0.0];  
-    %end
 
-    ax= sd063_cast_plots(ctds_plot(ii),cols,ref_station,plot_envelope);
+    ax= sd063_cast_plots(ctds_plot(ii),cols,line_style,ref_station,plot_envelope);
     hold on;
-
     ctd_time=datetime(ctds_plot(ii).gtime);
     ctds_times=[ctds_times ctd_time];
     cast_number=[cast_number P.sectionlist(ii)];
@@ -86,9 +99,12 @@ subplot(1,4,3:4)
 labels = string(ctds_times) + " (" + string(cast_number) + ")";
 legend(labels,'Location','northeast','FontSize',8);
 
+subplot(1,4,1)
+xlim=[P.tcaxis]; 
+
 %lgd=legend([string(ctds_times),(cast_number)],'Location','SouthWest','FontSize',8);
 %,string(ctds_times(3)),string(ctds_times(4)),string(ctds_times(5)),string(ctds_times(6)),string(ctds_times(7)),'Location','SouthWest','FontSize',8)
 lgd.ItemTokenSize = [12 10];
 
-name = sprintf('_ctd_casts2_%s.png', sectionfilename);
+name = sprintf('_ctd_casts_%s.png', sectionfilename{1});
 exportgraphics(gcf, fullfile('Figures', [cruise name]), 'Resolution', 300)
