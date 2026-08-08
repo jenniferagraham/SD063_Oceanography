6442b2f5a711673301fd4d024aa3e999c6adb8b6
Laura C <sa07lc@sams.ac.uk>
2026-08-08 09:54:18 +0100

    manage to get a broad and a focus map

--- MSS34/Processing/matscripts/matfile_to_structure.m
+++ MSS34/Processing/matscripts/matfile_to_structure.m
@@ -1,10 +1,35 @@
 close all; clear all;
-
-% Find all MSS files
-MSSDataP = 'L:\work\scientific_work_areas\oceanography\MSS34\DATA\';
-files = dir(fullfile(MSSDataP,'fasteps\','*eps.mat'));
-
+redo=1; % re do the structures 
+%% defien paths 
+if ispc
+    disk = 'L:\work\scientific_work_areas\';
+    MSSDataP = 'L:\work\scientific_work_areas\oceanography\MSS34\DATA\fasteps\';
+    MSSDataSave = 'L:\work\scientific_work_areas\oceanography\MSS34\DATA\';
+    msslogbook = [disk,'oceanography\MSS34\MSS_logbook_4matlab.csv'];
+    addpath([disk,'oceanography\CTD\GSWscripts\gsw_matlab_v3_06_16\thermodynamics_from_t\'])
+    addpath([disk,'oceanography\CTD\GSWscripts\gsw_matlab_v3_06_16\'])
+    addpath([disk,'oceanography\CTD\GSWscripts\gsw_matlab_v3_06_16/library\'])
+elseif ismac
+    disk = '/Volumes/leg/work/scientific_work_areas/';
+    MSSDataP = ['/Volumes/leg/work/scientific_work_areas/oceanography/MSS34/DATA/fasteps/'];
+    MSSDataSave = ['/Volumes/leg/work/scientific_work_areas/oceanography/MSS34/DATA/'];
+    msslogbook = [disk,'oceanography/MSS34/MSS_logbook_4matlab.csv']; 
+    addpath([disk,'oceanography/CTD/GSWscripts/gsw_matlab_v3_06_16/'])
+    addpath([disk,'oceanography/CTD/GSWscripts/gsw_matlab_v3_06_16/thermodynamics_from_t/'])
+    addpath([disk,'oceanography/CTD/GSWscripts/gsw_matlab_v3_06_16/library/'])
+end
+%% load log book to add lat and lon to structure variable
+mssLog = importMSSlogbook4matlab(msslogbook); % use =1 means event action 'inWater'
+%VariableNames = ["Time", "Latitude_dd", "Longitude_dd", "DepthEA640_m", "MSSstation", "EventNumber", "MSScast", "TargetDepth_m", "Comment", "Use"];
+indxRow = find(mssLog.Use==1); % inWater rows only
+% create work table
+msslogDepth = mssLog.DepthEA640_m(indxRow);
+msslogLon = mssLog.Longitude_dd(indxRow);
+msslogLat = mssLog.Latitude_dd(indxRow);
+mssLogMSScast = mssLog.MSScast(indxRow);
 %%
+files = dir(fullfile(MSSDataP,'*eps.mat'));
+
 if isempty(files)
     error('No MSS files found.');
 end
@@ -15,7 +40,7 @@ cast_numbers = zeros(length(files),1);
 for i = 1:length(files)
     % Filename example: SD630002_eps.mat
     % search for number in filename % export to tokens outkey
-    tokens = regexp(files(i).name,'SD6300(\d+)_','tokens'); 
+    tokens = regexp(files(i).name,'SD6300(\d+)_','tokens');
     cast_numbers(i) = str2double(tokens{1}{1});
 end
 
@@ -40,22 +65,36 @@ for cast = 1:max_cast
     end
 
     infile = files(idx).name;
-     outfile = sprintf('SD063_mss_%03d_struct.mat', cast);
-    
-     if exist(outfile,'file')
-     display(outfile, 'already exist => skipping');
+    outfile = sprintf('SD063_mss_%03d_struct.mat', cast);
+
+     if exist(outfile,'file') && redo==0
+         display(outfile, 'already exist => skipping');
      else
 
-    display(['Processing %s\n', infile]);
+        display(['Processing %s\n', infile]);
 
-    % Load original file
-    S = load(fullfile(MSSDataP,'fasteps\',infile));
-    S.station =cast; % add the cast number 
-    % Put all variables into mss structure
-    mss = S;
-    mss.data.corrsal = mss.data.sal - 0.080274;
-    % Save new structure file
-    save(fullfile(MSSDataP,outfile),'mss');
+        % Load original file
+        S = load(fullfile(MSSDataP,infile));
+
+        S.station =cast; % add the cast number
+        % Put all variables into mss structure
+        mss = S;
+        mss.data.corrsal = mss.data.sal - 0.080274;
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % adding the lat-lon value for each cast
+        castidx = find(mssLogMSScast==cast);
+        mss.lat = msslogLat(castidx); % using inwater - probe would have gone straight down
+        mss.lon = msslogLon(castidx); % using inwater
+        mss.depth = msslogDepth;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % add absolute salinity and conservative temperature
+
+        mss.data.asal =gsw_SA_from_SP(mss.data.corrsal,mss.data.press,mss.lat,mss.lon);
+        mss.data.ct  =gsw_CT_from_t(mss.data.corrsal,mss.data.temp,mss.data.press);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Save new structure file
+        save(fullfile(MSSDataSave,outfile),'mss');
     end
 end
 
