b030a03f4d8ed3e47d16e5df0a5f4a2170e0a0f7
Jennifer Graham <jennifer.graham@sams.ac.uk>
2026-08-11 20:42:03 -0100

    updated to add phase and save to figures and csv

--- CTD/Code/plot_tide_simple.m
+++ CTD/Code/plot_tide_simple.m
@@ -7,13 +7,20 @@ Tdisk = ['P:\SD063\']; % JG T-drive
 %Tdisk = ['T:\SD063\'];
 
 % location of interest? 
-location = 'all' % kg or melange
+location = 'fjordall'; % kg or melange
 timenow = false;
+savecsv = true;
 
 switch location
     case 'all'
         lat = [68.2796, 68.1085, 67.91221, 67.70328];
         lon = [-30.7665, -30.42815, -31.72869, -32.8002];
+        tstart = '22-Jul-2026';
+        tend = '22-Aug-2026';
+
+    case 'fjordall'
+        lat = 68.2796;
+        lon = -30.7665;
         tstart = '22-Jul-2026';
         tend = '22-Aug-2026';
 
@@ -62,7 +69,7 @@ addpath(fullfile(Tdisk,'TMD3.0'))
 %    tstart = 
 
 figure
-t = datetime(tstart):hours(0.25):datetime(tend);
+t = datetime(tstart):hours(5/60):datetime(tend);
 if strcmp(location, 'all')
     for ii=1:length(lon)
         z = tmd_predict(fullfile(Tdisk,'Gr1kmTM/data/Gr1kmTM_v1.nc'),...
@@ -86,25 +93,51 @@ grid on
 figname = sprintf('Tide_%s.png', location);
 exportgraphics(gcf,fullfile('..', figname),'Resolution',300)
 
+if savecsv
+    csvname = sprintf('Tide_%s.csv', location);
+    dzdt = gradient(z,hours(t(2)-t(1)));
+    T = table(t(:), z(:), dzdt(:), 'VariableNames', {'Time','Z', 'dZdt'});
+    writetable(T, fullfile('..', csvname));
+end
+
 %% Classify ebb and flood? 
-% 
-% tevent = % list of stations & time...
-% 
-% tevent = datetime(event_dates, 'TimeZone', 'UTC');
-% 
-% % Ensure tide times have a timezone too
-% t.TimeZone = 'UTC';
-% 
-% % Interpolate
-% zevent = interp1(t, z, tevent);
-% 
-% % zevent now contains tide height at each event time
-% 
-% % To consider flood vs ebb?
-% dzdt = gradient(z, hours(t(2)-t(1)));
-% dzdtevent = interp1(t, dzdt, tevent);
-% 
-% state = strings(size(tevent));
-% state(dzdtevent > 0) = "Flood";
-% state(dzdtevent < 0) = "Ebb";
-% 
+s = sign(T.dZdt);
+
+highIdx = find(diff(s) < 0) + 1;   % +ve to -ve
+lowIdx  = find(diff(s) > 0) + 1;   % -ve to +ve
+
+T.phase = repmat("flood",height(T),1);
+T.phase(T.dZdt < 0) = "ebb";
+
+% Set as high/low for +/- 20 min (4 x dt) around max/min 
+for ii=1:length(highIdx)
+    imax = highIdx(ii);
+    T.phase(imax-4:imax+4) = "high";
+end
+for ii=1:length(lowIdx)
+    imax = lowIdx(ii);
+    T.phase(imax-4:imax+4) = "low";
+end
+
+%%
+if savecsv
+    csvname = sprintf('Tide_phase_%s.csv', location);
+    writetable(T, fullfile('..', csvname));
+end
+
+%% Plot phase for today
+
+figure;
+scatter(T.Time(T.phase=='flood'),T.Z(T.phase=='flood'), 'r')
+hold on
+scatter(T.Time(T.phase=='ebb'),T.Z(T.phase=='ebb'), 'b')
+scatter(T.Time(T.phase=='high'),T.Z(T.phase=='high'), 'k', 'filled')
+scatter(T.Time(T.phase=='low'),T.Z(T.phase=='low'), 'y', 'filled')
+
+xlim([datetime('today') datetime('tomorrow')])
+grid on
+
+ylabel('tide height (m)')
+
+figname = sprintf('Tide_phase_%s_today.png', location);
+exportgraphics(gcf,fullfile('..', figname),'Resolution',300)
