905f1e36c88be23fc3fb1bebf95465683049e4da
Jennifer Graham <jennifer.graham@sams.ac.uk>
2026-08-13 13:41:31 -0100

    add tide height and phase to structure

--- CTD/plot_transects/sd063_ctds_structVAR.m
+++ CTD/plot_transects/sd063_ctds_structVAR.m
@@ -66,9 +66,7 @@ for ii = 1:length(stationlist)
         ctd.ladcp_v = nan;
 
     end
-    if ii==123
-        1;
-    end
+
     %%Day team needed this commented out line:
     %ctds(ctd.station) = ctd; % should I change the names?
    
@@ -77,7 +75,80 @@ for ii = 1:length(stationlist)
     %for the right station number....
     ctds(ii) = ctd; % should I change the names?
 
-
-
 end
+
+%Now add in tidal info based on the tidal model
+
+% allstations=[ctds.station];
+% stns=allstations;
+% ind=zeros(size(stns));
+% for n=1:length(stns)
+%     try
+%         ind(n)=find(allstations==stns(n));
+%     catch
+%         error('Cannot find %s station %d',cruise,stns(n));
+%     end
+% end
+% ctds=ctds(ind);
+
+%% All CTDs have a gtime number that needs converting to datenum:
+ctds_times=[];
+for ii=1:length(stationlist)
+    ctd_time=datetime(ctds(ii).gtime);
+    ctds_times=[ctds_times ctd_time];
+end
+
+%calculate tides for the range of interest:
+t = datetime('jul 16, 2026'):seconds(1):datetime('aug 28, 2026');
+z = tmd_predict('T:/SD063/Gr1kmTM/data/Gr1kmTM_v1.nc',68.2796,-30.7665,t);
+dzhdt=diff(z)/datenum(t(2)-t(1));
+time_at_deriv=t(:)+(t(2)-t(1))/2;
+
+
+%Output tide height zh at the time of the ctd:
+%% loop the sections 
+for ii=1:length(stationlist)
+    [~,index_zh]=min(abs(t-ctds_times(ii)));
+    ctds(ii).tide_height_zh=z(index_zh);
+    ctds(ii).tide_rising_rate_mpday=dzhdt(index_zh);
+end
+
+
+%Now find the first tide height
+[pks, loc] = findpeaks(z);
+peak_times=t(loc);
+%plot(peak_times, pks, 'ro')
+
+last_peak_time   = NaT(size(ctds_times));
+next_peak_time = NaT(size(ctds_times));
+last_peak_height = NaN(size(ctds_times));
+
+for i = 1:numel(ctds_times)
+
+    previous = find(peak_times < ctds_times(i));
+    % Peaks after the CTD cast
+    next = find(peak_times > ctds_times(i));
+
+    if ~isempty(previous)
+        idx = previous(end);
+        last_peak_time(i)   = peak_times(idx);
+        last_peak_height(i) = pks(idx);
+    end
+
+    if ~isempty(next)
+        next_peak_time(i) = peak_times(next(1));
+    end
+end
+
+% calculate peak duration:
+peak_duration = next_peak_time - last_peak_time;
+
+%Now calculate fraction of cycle the CDT cast was from the last maximum:
+% Loop over each structure (cast)
+for ii=1:length(stationlist)
+    frac = (ctds_times(ii) - last_peak_time(ii)) / peak_duration(ii);
+    ctds(ii).tide_phase_fraction=frac;
+end
+
+%save structure:
 save(fullfile(sd063_CTD, 'SD063_ctd.mat'),'ctds')
