ba609105e4c88cfbf78ff91be8bdbdf9001b4493
Jennifer Graham <jennifer.graham@sams.ac.uk>
2026-08-26 18:22:13 +0100

    section updates

--- MSS34/Processing/matscripts/sdaSectionParamsMSS.m
+++ MSS34/Processing/matscripts/sdaSectionParamsMSS.m
@@ -26,57 +26,55 @@ switch lower(sectionName)
         P.sectionname = '3-M All';
         P.maxy   = 400;
         P.tcaxis = [-2 2];
-    case '3minner_towyo'
-        P.castlist = flip([44:55]);
-        P.stationlist = flip([24:34]); % left to right facing the ice
-        P.sectionname = '3-M Inner towyo';
-        P.maxy   = 400; 
+    case  '3msill_towyo'
+        P.castlist = flip([31:43]);
+        P.stationlist = flip([12:24]); % left to right facing the ice
+        P.sectionname = '3-M Sill towyo';
+        P.maxy   = 250; 
         P.vcaxis = [-0.3 0.3];
         P.tcaxis = [-2 1];
         P.scaxis = [26.5 34.5];
-   case '3minner'
+    case '3minner_towyo' % ice front towyo across fjord transect in front of the ice face 
+        P.castlist = flip([44:55]);
+        P.stationlist = flip([24:34]); % left to right facing the ice
+        P.sectionname = '3-M Inner towyo';
+        P.maxy   = 250; 
+        P.vcaxis = [-0.3 0.3];
+        P.tcaxis = [-2 1];
+        P.scaxis = [26.5 34.5];
+    case '3minner_mss' % across fjord transect with multiple cast per station, also at the ice front. 
         P.castlist=[7:16];
         P.stationlist = [3 3 3 4 4 4 5 6 6 6]; % station list 
         P.sectionname = '3-M Inner';
         P.maxy   = 400; 
         P.tcaxis = [-2 1];
         P.scaxis = [26.5 34.5];
-    case '3mtransect' % along fjord transect
+ 
+    case '3mtransect_mss' % along fjord transect inner
         P.fjord = 1;
-        P.stationlist = [];
-        P.castlist = [];
-        P.sectionname = '3-M Transect';
-        P.maxy   = 500; 
-        P.vcaxis = [-0.3 0.3];
-        P.tcaxis = [-2 1];
-        P.scaxis = [26.5 34.5];
-        P.mLON   = [-31 -30];
-        P.mLAT   = [68 68.5];
-    case '3mtransectmss' % along fjord transect inner
-        P.fjord = 1;
-        P.stationlist = [6 , 6,   6, 2, 2, 2,  7,  7,  8,  8, 9,   9,  9, 14];
-        P.castlist =    [14, 15, 16, 4, 5, 6, 17,  18, 19, 20, 21, 22, 23, 33]; % cast 19 replaced a cast with an identical no. to 20 
+       % P.stationlist = [6 , 6,   6, 2, 2, 2,  7,  7,  8,  8, 9,   9,  9, 14];
+       % P.castlist =    [14, 15, 16, 4, 5, 6, 17,  18, 19, 20, 21, 22, 23, 33]; % cast 19 replaced a cast with an identical no. to 20 
+        P.stationlist = [6 , 6,   6, 7,  7,  8,  8, 9,   9,  9, 14];
+        P.castlist =    [14, 15, 16, 17,  18, 19, 20, 21, 22, 23, 33]; % cast 19 replaced a cast with an identical no. to 20 
         P.sectionname = '3-M Transect inner to Sill';
-        P.maxy   = 500; 
+        P.maxy   = 300; 
         P.vcaxis = [-0.3 0.3];
         P.tcaxis = [-2 1];
         P.scaxis = [26.5 34.5];
         P.mLON   = [-31 -30];
         P.mLAT   = [68 68.5];
-    case '3msill_towyo'
+    case '3mtransect_inner_mss' % along fjord transect inner (short)
         P.fjord = 1;
-        P.stationlist = flip([12:24]); % left to right 
-        % ***** 16 **** is repeated - needds to be fix by alice delete this message when it is fixed. 
-        P.castlist = flip([31:43]); % left to right is the plotting convention.
-        P.sectionname = '3-M Sill towyo';
-        P.maxy   = 250; 
-        P.fjord  = 1;
+        P.stationlist = [05, 11, 11, 11, 11];
+        P.castlist =    [13, 27, 28, 29, 30]; % cast 19 replaced a cast with an identical no. to 20 
+        P.sectionname = '3-M Transect inner to Sill';
+        P.maxy   = 300; 
         P.vcaxis = [-0.3 0.3];
         P.tcaxis = [-2 1];
         P.scaxis = [26.5 34.5];
         P.mLON   = [-31 -30];
         P.mLAT   = [68 68.5];
-        P.clevels = [24:0.5:28];
+   
     otherwise
         error('Unknown section name: %s',sectionName)
 
